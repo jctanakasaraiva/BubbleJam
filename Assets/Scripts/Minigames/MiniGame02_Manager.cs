5d7d93c8a99496bcd62d2642fa517f6bab104e4a
YALLiS <yallis.sousa@gmail.com>
2025-01-24 13:30:22 -0400

    atribuicao de sprites

--- Assets/Scripts/Minigames/MiniGame02_Manager.cs
+++ Assets/Scripts/Minigames/MiniGame02_Manager.cs
@@ -8,21 +8,22 @@ public class MiniGame02_Manager : MonoBehaviour {
     public enum levels { Level_1, Level_2, Level_3, Level_4, Level_5 };
     public levels Dificuldade;
 
-    public Image barImg;
+    public Transform pointA;
+    public Transform pointB;
+    private float soapSpeed = 5f;
+
     public GameObject target;
     public GameObject bubble;
     public GameObject explosion;
 
-    float timeMax = 1.5f;
-    float timeleft;
     float levelFactor = 1f;
-    bool crescendo = false;
     bool clicavel = true;
 
     bool taNoTarget = false;
     bool loseFlag = false;
-    float scaleFactor = 1f;
     Vector2 newScale;
+
+    private Vector3 nextPosition;
 
     private ManagerMinigames manager;
 
@@ -30,27 +31,27 @@ public class MiniGame02_Manager : MonoBehaviour {
 
         switch (Dificuldade) {
             case levels.Level_1:
-                levelFactor = 1f;
+                levelFactor = 2.25f;
                 break;
             case levels.Level_2:
-                levelFactor = 1.25f;
+                levelFactor = 2f;
                 break;
             case levels.Level_3:
-                levelFactor = 1.5f;
-                break;
-            case levels.Level_4:
                 levelFactor = 1.75f;
                 break;
+            case levels.Level_4:
+                levelFactor = 1.5f;
+                break;
             case levels.Level_5:
-                levelFactor = 2f;
+                levelFactor = 1.25f;
                 break;
             default:
                 Debug.Log("Ué");
                 break;
         }
 
-        //Muda o tamanho da zona verde conforme dificuldade do level
-        target.transform.localScale = new Vector2(target.transform.localScale.x / levelFactor, target.transform.localScale.y);
+        //Muda o tamanho do target conforme dificuldade do level
+        target.transform.localScale = new Vector2(target.transform.localScale.x / (2/levelFactor), target.transform.localScale.y);
         explosion.SetActive(false);
     }
 
@@ -58,41 +59,24 @@ public class MiniGame02_Manager : MonoBehaviour {
         manager = this.gameObject.GetComponent<ManagerMinigames>();
 
         newScale = bubble.transform.localScale;
-        timeleft = timeMax;
-    }
-
-    private void FixedUpdate() {
-        if (manager.Runnig) {
-
-            if ( !crescendo ) {
-                if (timeleft > 0) {
-                    timeleft -= Time.deltaTime;
-                } else crescendo = true;
-            }
-            else if (timeleft < timeMax) {
-                timeleft += Time.deltaTime;
-            } else crescendo= false;
-
-        
-            scaleFactor = timeleft / timeMax;
-            barImg.transform.localScale = new Vector2( scaleFactor, barImg.transform.localScale.y );
-
-            bubble.transform.localScale = Vector2.Lerp(bubble.transform.localScale, newScale, .1f);
-        }
+        nextPosition = pointB.position;
+        soapSpeed *= 2 / levelFactor;
     }
 
     private void Update() {
         if (manager.Runnig) {
 
             if (Input.GetMouseButtonDown(0)) {
-                if (taNoTarget & clicavel) {
-                    newScale *= 1f + scaleFactor / levelFactor;
-                    clicavel = false;
+                if (taNoTarget) {
+                    if (clicavel) {
+                        newScale *= 1 + 1/levelFactor;
+                        clicavel = false;
 
-                    if (newScale.x > 3) loseFlag = true;
+                        if (newScale.x > 3) loseFlag = true;
+                    }
                 }
                 else {
-                    newScale /= 1f + scaleFactor;
+                    newScale /= 1 + 1/levelFactor;
                 }
             }
 
@@ -102,6 +86,14 @@ public class MiniGame02_Manager : MonoBehaviour {
             else if ((newScale.x < 3 & loseFlag) || newScale.x < 0.5f) {
                 Lose();
             }
+
+            transform.position = Vector2.MoveTowards(transform.position, nextPosition, soapSpeed * Time.deltaTime);
+            if(transform.position.x == nextPosition.x) {
+                nextPosition = (nextPosition.x == pointA.position.x) ? pointB.position : pointA.position;
+            }
+
+            //Escala a bolha
+            bubble.transform.localScale = Vector2.Lerp(bubble.transform.localScale, newScale, .1f);
         }
     }
 
